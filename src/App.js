178d7abcaf29005ecec7eace85356f70e0109139
Livnat Arama <livnatar@std.hac.ac.il>
2025-04-22 16:28:33 +0300

    create Header directory

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Header from "./components/Header";
+import Header from "./components/header/Header";
 import FavoriteCities from "./components/FavoriteCities";
 import About from "./components/About";
 import NotFound from "./components/NotFound";
---
+++ src/components/header/AppTitle.js
@@ -0,0 +1,9 @@
+export default function AppTitle() {
+    return (
+        <div className="bg-primary text-white py-3">
+            <div className="container">
+                <h1>Your App Name</h1>
+            </div>
+        </div>
+    );
+}
