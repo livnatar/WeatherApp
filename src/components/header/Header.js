dda39eb97b67610e3fe9798eddc2cff089a96fdc
Livnat Arama <livnatar@std.hac.ac.il>
2025-04-27 11:21:02 +0300

    fix to container fluid

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,3 @@
-// import {Link} from 'react-router-dom';
-// import {Outlet} from "react-router";
-//
-// export default function Header(props) {
-//
-//     return (
-//         <div>
-//             <h1>An example of browser-router</h1>
-//
-//             <Link to="/">Favorite Cities</Link> {' '} | {' '}
-//             <Link to="/AllCities">All Cities</Link> {' '} | {' '}
-//             <Link to="/About">About</Link>
-//
-//             {/* An <Outlet> renders whatever child route is currently active,
-//           so you can think about this <Outlet> as a placeholder for
-//           the child routes we defined above. */}
-//             <Outlet />
-//
-//         </div>
-//     );
-// }
 
 import { Outlet } from "react-router";
 import AppTitle from './AppTitle';
