a21c20498cf8fba3388a9287c6b306098b71a086
Ophek Alon <ophekal@std.hac.ac.il>
2025-04-27 21:56:14 +0300

    changed app logo and background

--- src/components/header/AppTitle.js
+++ src/components/header/AppTitle.js
@@ -1,16 +1,30 @@
-
-import { Container, Row, Col } from 'react-bootstrap';
+import {Container, Row, Col} from 'react-bootstrap';
+import logo from '../../imgs/logo.png';
 
 export default function AppTitle() {
     return (
-        <div className="bg-primary py-3">
+        <div className="bg-light-primary py-3">
             <Container fluid>
-                <Row className="justify-content-center">
-                    <Col md={8} className="text-center">
-                        <h1 className="text-white">Weather App</h1>
+                <Row className="justify-content-center align-items-center">
+                    <Col md={8} className="d-flex justify-content-center align-items-center text-center">
+                        <img
+                            src={logo}
+                            alt="Logo"
+                            className="mr-3"
+                            style={{width: '80px', height: '80px', marginRight: '15px'}}
+                        />
+                        <h1 style={{ color: '#6f4f1f' }}>Weather App</h1>
                     </Col>
                 </Row>
             </Container>
         </div>
     );
 }
+//<h1 className="text-white">Weather App</h1>
+
+//
+// <Row className="justify-content-center">
+//     <Col md={8} className="text-center">
+//         <h1 className="text-dark">Weather App</h1>
+//     </Col>
+// </Row>
